485df23bfe084eb8583d9f4670bf6cd4541312ee
IMH51 <ianmartinharrison@outlook.com>
2018-10-31 16:45:41 +0000

    all tests pass

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    keys = []
+    arguments.each do |argument|
+      self.each do |key, value|
+        if value == argument
+          keys << key
+        end
+      end
+    end
+    return keys
   end
 end
